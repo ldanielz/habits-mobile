a243e398a19fa7c5aca32530c84ab01ec3f52835
Lizandro Daniel <ldaniel.eng@gmail.com>
2023-01-23 21:20:10 -0300

    feat: disabled past habit

--- src/screens/Habit.tsx
+++ src/screens/Habit.tsx
@@ -10,6 +10,7 @@ import { ProgressBar } from '../components/Progress.Bar'
 import { api } from '../lib/axios'
 import { generateProgressPercentage } from '../utils/generate-progress-percentage'
 import { HabitsEmpty } from '../components/HabitsEmpty'
+import clsx from 'clsx'
 
 interface Params {
   date: string
@@ -31,6 +32,7 @@ export function Habit() {
   const { date } = route.params as Params
 
   const parsedDate = dayjs(date)
+  const isDateInPast = parsedDate.endOf('day').isBefore(new Date())
   const dayOfWeek = parsedDate.format('dddd')
   const dayAndMonth = parsedDate.format('DD/MM')
 
@@ -96,7 +98,11 @@ export function Habit() {
 
         <ProgressBar progress={habitsProgress} />
 
-        <View className="mt-6">
+        <View
+          className={clsx('mt-6', {
+            'opacity-50': isDateInPast,
+          })}
+        >
           {dayInfo?.possibleHabits.length ? (
             dayInfo?.possibleHabits.map((habit) => (
               <Checkbox
@@ -104,12 +110,18 @@ export function Habit() {
                 title={habit.title}
                 checked={completedHabits.includes(habit.id)}
                 onPress={() => handleToggleHabit(habit.id)}
+                disabled={isDateInPast}
               />
             ))
           ) : (
             <HabitsEmpty />
           )}
         </View>
+        {isDateInPast && (
+          <Text className="text-white mt-10 text-center">
+            Voce nao pode editar habitos passados
+          </Text>
+        )}
       </ScrollView>
     </View>
   )
